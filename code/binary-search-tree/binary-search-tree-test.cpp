351f128cbd1fad180a567aa38eba67cb0fd958f9
Yuanhang <zhengyhn@gmail.com>
2013-10-03 16:06:16 +0800

    Site updated at 2013-10-03 08:06:16 UTC

--- code/binary-search-tree/binary-search-tree-test.cpp
+++ code/binary-search-tree/binary-search-tree-test.cpp
@@ -5,12 +5,15 @@
 #include <boost/test/output_test_stream.hpp>
 #include "binary-search-tree.hpp"
 
-BOOST_AUTO_TEST_SUITE(create)
+BOOST_AUTO_TEST_SUITE(type)
 
 BOOST_AUTO_TEST_CASE(empty)
 {
     BinarySearchTree<int> tree;
     BOOST_CHECK(tree.is_empty());
+    tree.insert(2);
+    BOOST_CHECK(!tree.is_empty());
+    BOOST_CHECK_EQUAL(tree.root(), 2);
 }
 
 BOOST_AUTO_TEST_CASE(one_node)
@@ -18,16 +21,27 @@ BOOST_AUTO_TEST_CASE(one_node)
     BinarySearchTree<int> tree(2);
     BOOST_CHECK_EQUAL(tree.root(), 2);
     BOOST_CHECK(!tree.is_empty());
+    tree.remove(2);
+    BOOST_CHECK(tree.is_empty());
 }
 
 BOOST_AUTO_TEST_CASE(from_array)
 {
-    char nodes[10] = {'c', 'b', 'd', 'a', 'f', 'e', 'g', 'h', 'i', 'j'};
+    char nodes[10] = {'c', 'b', 'd', 'a', 'f', 'e', 'g', 'h', 'i', 'b'};
     BinarySearchTree<char> tree(nodes, 10);
     BOOST_CHECK(!tree.is_empty());
     BOOST_CHECK_EQUAL(tree.root(), 'c');
-    std::string expect_inorder = "[a, b, c, d, e, f, g, h, i, j]";
-    BOOST_CHECK_EQUAL(tree.inorder_travel(), expect_inorder);
+    std::string expect_inorder = "[a, b, b, c, d, e, f, g, h, i]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+
+    std::vector<char> result = tree.retrieve('b');
+    BOOST_CHECK_EQUAL(result.size(), 2);
+    BOOST_CHECK_EQUAL(result[0], 'b');
+    BOOST_CHECK_EQUAL(result[1], 'b');
+    
+    tree.remove('b');
+    expect_inorder = "[a, c, d, e, f, g, h, i]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
 }
 
 BOOST_AUTO_TEST_CASE(copy)
@@ -40,7 +54,10 @@ BOOST_AUTO_TEST_CASE(copy)
     t2.insert(1);
     BinarySearchTree<int> t3 = t2;
     std::string expect_inorder = "[1, 2, 3]";
-    BOOST_CHECK_EQUAL(t3.inorder_travel(), expect_inorder);
+    BOOST_CHECK_EQUAL(t3.inorder_string(), expect_inorder);
+
+    t3.remove(3);
+    BOOST_CHECK_EQUAL(t3.inorder_string(), "[1, 2]");
 }
 
 BOOST_AUTO_TEST_CASE(assign)
@@ -59,7 +76,158 @@ BOOST_AUTO_TEST_CASE(assign)
     t3.insert(4);
     t1 = t2 = t3;
     std::string expect_inorder = "[1, 2, 3, 4]";
-    BOOST_CHECK_EQUAL(t1.inorder_travel(), expect_inorder);
+    BOOST_CHECK_EQUAL(t1.inorder_string(), expect_inorder);
+
+    t1.remove(3);
+    BOOST_CHECK_EQUAL(t1.inorder_string(), "[1, 2, 4]");
+}
+
+BOOST_AUTO_TEST_CASE(remove)
+{
+    char nodes[10] = {'c', 'b', 'd', 'a', 'f', 'e', 'g', 'h', 'i', 'b'};
+    BinarySearchTree<char> tree(nodes, 10);
+
+    tree.remove('f');
+    std::string expect_inorder = "[a, b, b, c, d, e, g, h, i]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+
+    tree.remove('i');
+    expect_inorder = "[a, b, b, c, d, e, g, h]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+
+    tree.remove('a');
+    expect_inorder = "[b, b, c, d, e, g, h]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+
+    tree.remove('c');
+    expect_inorder = "[b, b, d, e, g, h]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+
+    tree.remove('g');
+    expect_inorder = "[b, b, d, e, h]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+
+    tree.remove('d');
+    expect_inorder = "[b, b, e, h]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+
+    tree.remove('h');
+    expect_inorder = "[b, b, e]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+
+    tree.remove('e');
+    expect_inorder = "[b, b]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+
+    tree.remove('b');
+    expect_inorder = "[]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(record)
+
+class Record {
+public:
+    explicit Record()
+        :key_(0), value_("")
+    { }
+
+    explicit Record(int key, const std::string& value="")
+        :key_(key), value_(value)
+    { }
+
+    explicit Record(const Record& record)
+        :key_(record.key_), value_(record.value_)
+    { }
+
+    const Record&
+    operator=(const Record& record)
+    {
+        this->key_ = record.key_;
+        this->value_ = record.value_;
+
+        return *this;
+    }
+
+    ~Record() { }
+
+    friend bool
+    operator<(const Record& left, const Record& right)
+    { return left.key_ < right.key_; }
+
+    friend bool
+    operator<=(const Record& left, const Record& right)
+    { return left.key_ <= right.key_; }
+
+    friend bool
+    operator==(const Record& left, const Record& right)
+    { return left.key_ == right.key_; }
+
+    friend bool
+    operator>=(const Record& left, const Record& right)
+    { return left.key_ >= right.key_; }
+
+    friend bool
+    operator>(const Record& left, const Record& right)
+    { return left.key_ > right.key_; }
+
+    void
+    key(int k)
+    { this->key_ = k; }
+
+    void
+    value(const std::string& v)
+    { this->value_ = v; }
+
+    const std::string&
+    value() const
+    { return this->value_; }
+    
+    friend std::ostream&
+    operator<<(std::ostream& os, const Record& record)
+    { return (os << record.value_); }
+private:
+    int key_;
+    std::string value_;
+};
+
+std::string names[5] = {"Dennis Ritchie", "Bjarne Stroustrup", "Don Knuth",
+                        "Linus Torvald", "Richard Stallman"};
+
+BOOST_AUTO_TEST_CASE(search)
+{
+    BinarySearchTree<Record> tree;
+    BOOST_CHECK(tree.is_empty());
+    Record record(2, names[1]);
+    tree.insert(record);
+    BOOST_CHECK_EQUAL(tree.root(), record);
+    record.key(3);
+    record.value(names[2]);
+    tree.insert(record);
+    
+    record.key(5);
+    record.value(names[4]);
+    tree.insert(record);
+
+    record.key(1);
+    record.value(names[0]);
+    tree.insert(record);
+
+    record.key(4);
+    record.value(names[3]);
+    tree.insert(record);
+
+    std::string expect_inorder = "[Dennis Ritchie, Bjarne Stroustrup, "
+        "Don Knuth, Linus Torvald, Richard Stallman]";
+    BOOST_CHECK_EQUAL(tree.inorder_string(), expect_inorder);
+
+    Record key(2);
+    std::vector<Record> result = tree.retrieve(key);
+    BOOST_CHECK_EQUAL(result.size(), 1);
+    BOOST_CHECK_EQUAL(result[0].value(), "Bjarne Stroustrup");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- code/binary-search-tree/binary-search-tree.hpp
+++ code/binary-search-tree/binary-search-tree.hpp
@@ -4,16 +4,18 @@
 #include <iostream>
 #include <sstream>
 #include <functional>
+#include <vector>
 
 template <typename T>
 class Node {
 public:
     explicit Node()
-        :value_(), lch_(NULL), rch_(NULL)
+        :value_(), lch_(NULL), rch_(NULL), parent_(NULL)
     { }
 
-    explicit Node(const T& value, Node<T> *lch=NULL, Node<T> *rch=NULL)
-        :value_(value), lch_(lch), rch_(rch)
+    explicit Node(const T& value, Node<T> *lch=NULL,
+                  Node<T> *rch=NULL, Node<T> *parent=NULL)
+        :value_(value), lch_(lch), rch_(rch), parent_(parent)
     { }
 
     ~Node() { }
@@ -21,6 +23,10 @@ public:
     const T&
     value() const
     { return this->value_; }
+
+    void
+    value(const T& v)
+    { this->value_ = v; }
 
     Node<T> *
     left_child() const
@@ -38,6 +44,14 @@ public:
     right_child(Node<T> *rch)
     { this->rch_ = rch; }
 
+    Node<T> *
+    parent() const
+    { return this->parent_; }
+    
+    void
+    parent(Node<T> *parent)
+    { this->parent_ = parent; }
+
     friend std::ostream
     operator<<(std::ostream& os, const Node<T>& node)
     { return os << node.value(); }
@@ -49,6 +63,7 @@ private:
     T value_;
     Node<T> *lch_;
     Node<T> *rch_;
+    Node<T> *parent_;
 };
 
 template <typename T>
@@ -125,6 +140,7 @@ public:
             if (value < p->value()) {
                 if (p->left_child() == NULL) {
                     Node<T> *node = new Node<T>(value);
+                    node->parent(p);
                     p->left_child(node);
                     break;
                 } else {
@@ -132,11 +148,14 @@ public:
                 }
             } else if (value == p->value()) {
                 Node<T> *node = new Node<T>(value, p->left_child());
+                node->left_child()->parent(node);
+                node->parent(p);
                 p->left_child(node);
                 break;
             } else {
                 if (p->right_child() == NULL) {
                     Node<T> *node = new Node<T>(value);
+                    node->parent(p);
                     p->right_child(node);
                     break;
                 } else {
@@ -147,32 +166,44 @@ public:
     }
 
     std::string
-    inorder_travel() const
+    inorder_string() const
     {
-        std::string result = inorder(this->root_);
-        result.erase(0, 1);
-        result[0] = '[';
+        std::string result = this->inorder_str(this->root_);
+        if (result != "") {
+            result.erase(0, 1);
+            result[0] = '[';        
+        } else {
+            result.append(1, '[');
+        }
         result.append(1, ']');
         
         return result;
     }
+
+    std::vector<T>
+    retrieve(const T& key)
+    { return this->search(this->root_, key); }
+
+    void
+    remove(const T& key)
+    { this->delete_nodes(this->root_, key, false); }
 private:
     Node<T> *root_;
     
     std::string
-    inorder(Node<T> *root) const
+    inorder_str(Node<T> *root) const
     {
         std::string result = "";
         std::string seperator = ", ";
         if (root != NULL) {
             if (root->left_child() != NULL) {
-                result += inorder(root->left_child());
+                result += this->inorder_str(root->left_child());
             }
             std::stringstream ss;
             ss << seperator << root->value();
             result += ss.str();
             if (root->right_child() != NULL) {
-                result += inorder(root->right_child());
+                result += this->inorder_str(root->right_child());
             }
         }
         return result;
@@ -183,10 +214,10 @@ private:
     {
         if (root != NULL) {
             if (root->left_child() != NULL) {
-                postorder(root->left_child(), func);
+                this->postorder(root->left_child(), func);
             }
             if (root->right_child() != NULL) {
-                postorder(root->right_child(), func);
+                this->postorder(root->right_child(), func);
             }
             func(root);
         }
@@ -201,11 +232,33 @@ private:
     }
 
     void
-    inorder(Node<T> *root, const std::function<void(const T&)>& func)
+    inorder(Node<T> *root, const std::function<void(void)>& func)
     {
-
+        if (root != NULL) {
+            if (root->left_child() != NULL) {
+                this->inorder(root->left_child(), func);
+            }
+            func(root);
+            if (root->right_child() != NULL) {
+                this->inorder(root->right_child(), func);
+            }
+        }
     }
 
+    void
+    preorder(Node<T> *root, const std::function<void(void)>& func)
+    {
+        if (root != NULL) {
+            func(root);
+            if (root->left_child() != NULL) {
+                this->preorder(root->left_child(), func);
+            }
+            if (root->right_child() != NULL) {
+                this->preorder(root->right_child(), func);
+            }
+        }
+    }
+    
     Node<T> *
     new_nodes(Node<T> *dest, const Node<T> *src)
     {
@@ -214,15 +267,123 @@ private:
                 dest = new Node<T>(src->value());
             }
             if (src->left_child() != NULL) {
-                dest->left_child(new_nodes(dest->left_child(),
-                                           src->left_child()));
+                Node<T> *node = new_nodes(dest->left_child(),
+                                          src->left_child());
+                node->parent(dest);
+                dest->left_child(node);
             }
             if (src->right_child() != NULL) {
-                dest->right_child(new_nodes(dest->right_child(),
-                                            src->right_child()));
+                Node<T> *node = new_nodes(dest->right_child(),
+                                          src->right_child());
+                node->parent(dest);
+                dest->right_child(node);
             }
         }
         return dest;
+    }
+
+    std::vector<T>
+    search(Node<T> *root, const T& key)
+    {
+        std::vector<T> values;
+        if (root != NULL) {
+            if (key == root->value()) {
+                values.push_back(root->value());
+            }
+            if (root->left_child() != NULL &&
+                root->left_child()->value() > key) {
+                return values;
+            }
+            if (key <= root->value()) {
+                std::vector<T> tmp =
+                    this->search(root->left_child(), key);
+                values.reserve(values.size() + tmp.size());
+                values.insert(values.end(), tmp.begin(), tmp.end());
+            }
+            if (root->right_child() != NULL &&
+                root->right_child()->value() < key) {
+                return values;
+            }
+            if (key > root->value()) {
+                std::vector<T> tmp =
+                    this->search(root->right_child(), key);
+                values.reserve(values.size() + tmp.size());             
+                values.insert(values.end(), tmp.begin(), tmp.end());
+            }
+        }
+        return values;
+    }
+
+    void
+    delete_nodes(Node<T> *root, const T& key, bool single)
+    {
+        if (root != NULL) {
+            if (key < root->value()) {
+                this->delete_nodes(root->left_child(), key, single);
+            } else if (key > root->value()) {
+                this->delete_nodes(root->right_child(), key, single);
+            } else {
+                while (key == root->value()) {
+                    if (root->left_child() != NULL &&
+                        root->right_child() != NULL) {
+                        Node<T> *predecessor =
+                            this->mini_node(root->right_child());
+                        root->value(predecessor->value());
+                        this->delete_nodes(predecessor, predecessor->value(),
+                                           true);
+                    } else if (root->left_child() != NULL) {
+                        if (root == this->root_) {
+                            this->root_ = root->left_child();
+                        } else if (root->parent()->left_child() == root) {
+                            root->parent()->left_child(root->left_child());
+                            root->left_child()->parent(root->parent());
+                        } else {
+                            root->parent()->right_child(root->left_child());
+                            root->left_child()->parent(root->parent());
+                        }
+                        Node<T> *to_delete = root;
+                        root = root->left_child();
+                        delete to_delete;
+                    } else if (root->right_child() != NULL) {
+                        if (root == this->root_) {
+                            this->root_ = root;
+                        } else if (root->parent()->left_child() == root) {
+                            root->parent()->left_child(root->right_child());
+                            root->right_child()->parent(root->parent());    
+                        } else {
+                            root->parent()->right_child(root->right_child());
+                            root->right_child()->parent(root->parent());   
+                        }
+                        Node<T> *to_delete = root;
+                        root = root->right_child();
+                        delete to_delete;
+                    } else {
+                        Node<T> *to_delete = root;
+                        if (this->root_ == root) {
+                            this->root_ = NULL;
+                        } else if (root->parent()->left_child() == root) {
+                            root->parent()->left_child(NULL);
+                        } else {
+                            root->parent()->right_child(NULL);
+                        }
+                        delete to_delete;
+                    }
+                    if (single) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    Node<T> *
+    mini_node(Node<T> *root) const
+    {
+        Node<T> *min = root;
+        while (min != NULL && min->left_child() != NULL) {
+            min = min->left_child();
+        }
+        return min;
     }
 };
 
