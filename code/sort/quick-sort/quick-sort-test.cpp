45af07fc1b78eadced2c97e8c8206b7dc58090df
Yuanhang <zhengyhn@gmail.com>
2013-10-04 23:59:30 +0800

    Site updated at 2013-10-04 15:59:30 UTC

--- code/sort/quick-sort/quick-sort-test.cpp
+++ code/sort/quick-sort/quick-sort-test.cpp
@@ -4,7 +4,7 @@
 #include <cstdlib>
 #include <algorithm>
 #include <boost/test/unit_test.hpp>
-#include "select-sort.cpp"
+#include "quick-sort.cpp"
 
 int empty_list[0];
 
@@ -15,16 +15,16 @@ int normal_list_sorted_descend[NORMAL_SIZE] = {8, 5, 4, 2, 1};
 
 const int RANDOM_SIZE = 9999;
 
-BOOST_AUTO_TEST_SUITE(select_)
+BOOST_AUTO_TEST_SUITE(quick_recursive)
 
 BOOST_AUTO_TEST_CASE(empty)
 {
-    select_sort(empty_list, 0);
+    qsort_r(empty_list, 0, 0);
 }
 
 BOOST_AUTO_TEST_CASE(normal)
 {
-    select_sort(normal_list, NORMAL_SIZE);
+    qsort_r(normal_list, 0, NORMAL_SIZE);
     for (int i = 0; i < NORMAL_SIZE; ++i) {
         BOOST_CHECK_EQUAL(normal_list[i], normal_list_sorted[i]);
     }
@@ -32,7 +32,7 @@ BOOST_AUTO_TEST_CASE(normal)
 
 BOOST_AUTO_TEST_CASE(normal_descending)
 {
-    select_sort(normal_list, NORMAL_SIZE, false);
+    qsort_r(normal_list, 0, NORMAL_SIZE, false);
     for (int i = 0; i < NORMAL_SIZE; ++i) {
         BOOST_CHECK_EQUAL(normal_list[i], normal_list_sorted_descend[i]);
     }
@@ -46,13 +46,12 @@ BOOST_AUTO_TEST_CASE(random)
     for (int i = 0; i < RANDOM_SIZE; ++i) {
         random_list[i] = random_list_sorted[i] = rand();
     }
-    select_sort(random_list, RANDOM_SIZE);
+    qsort_r(random_list, 0, RANDOM_SIZE);
     std::sort(random_list_sorted, random_list_sorted + RANDOM_SIZE);
     for (int i = 0; i < RANDOM_SIZE; ++i) {
         BOOST_CHECK_EQUAL(random_list[i], random_list_sorted[i]);
     }
 }
-
 
 BOOST_AUTO_TEST_SUITE_END()
 
