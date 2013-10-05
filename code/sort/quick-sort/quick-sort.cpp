197a217be454fa6f8fb641d9edb65948c52a0174
Yuanhang <zhengyhn@gmail.com>
2013-10-05 09:04:09 +0800

    Site updated at 2013-10-05 01:04:09 UTC

--- code/sort/quick-sort/quick-sort.cpp
+++ code/sort/quick-sort/quick-sort.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 template <typename T>
-void
+inline void         // Inline, may be faster.
 swap(T& a, T& b)
 {
+    if (a == b) {  // If the same, there's no need to swap.
+        return;
+    }
     T tmp = a;
     a = b;
     b = tmp;
@@ -13,25 +16,29 @@ template <typename T>
 void
 qsort_r(T arr[], int begin, int end, bool is_ascending=true)
 {
-    if (arr == NULL || begin >= end - 1) {
+    if (arr == NULL || begin >= end - 1) {   // If no element or one element.
         return;
     }
-    
-    int pivot = end - 1;
-    int last_left = begin - 1;
+    // First swap the middle element with the last element.
+    swap(arr[(begin + end - 1) / 2], arr[end - 1]);
+
+    int pivot = end - 1;      // That is, the pivot is the middle element.
+    int last_left = begin - 1;   // The index of the last element in the left
+
     for (int i = begin; i < end - 1; ++i) {
         if ((is_ascending && arr[i] < arr[pivot]) ||
             (!is_ascending && arr[i] > arr[pivot])) {
-            ++last_left;
-            if (i != last_left) {
-                swap(arr[i], arr[last_left]);
-            }
+            swap(arr[i], arr[++last_left]);
         }
     }
-    ++last_left;
-    if (pivot != last_left) {
-        swap(arr[pivot], arr[last_left]);
-    }
+    swap(arr[pivot], arr[++last_left]);
+
     qsort_r(arr, begin, last_left, is_ascending);
     qsort_r(arr, last_left + 1, end, is_ascending);
 }
+
+template <typename T>
+void
+qsort_i(T arr[], int begin, int end, bool is_ascending=true)
+{
+    
